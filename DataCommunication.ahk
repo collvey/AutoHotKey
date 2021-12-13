9c6192f7801b4d1a4e294148837667acfacf2097
collvey <collvey@umd.edu>
2021-12-13 00:05:38 -0800

    Add DataCommunication scripts to test communication of data between AHK and Python

---
+++ DataCommunication.ahk
@@ -0,0 +1,4 @@
+#Persistent
+data := "Hello Data!"
+FileAppend, %data%, * ; send data var to stdout
+ExitApp
---
+++ DataCommunication.py
@@ -0,0 +1,6 @@
+from ahk import AHK
+
+if __name__ == "__main__":
+    ahk = AHK()
+    result = ahk.run_script("DataCommunication.ahk")
+    print(result)
