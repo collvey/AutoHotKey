e7e1dc65abfa2143e6947d0a0fcb23bd8a09e6cc
collvey <collvey@umd.edu>
2021-12-12 22:44:03 -0800

    Add support for pandas in HelloWorld script

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 
 if __name__ == "__main__":
   print("Hello World")
-  print(np.__version__)
+  print(np.__version__) # 1.21.4 as of 12/12/2021
+  print(pd.__version__) # 1.3.5 as of 12/12/2021
