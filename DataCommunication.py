4c81cf9396399e679712e1fb2a78f5713240ba1f
collvey <collvey@umd.edu>
2021-12-13 00:10:23 -0800

    Example to communicate data between ahk script and Python

--- DataCommunication.py
+++ DataCommunication.py
@@ -2,5 +2,9 @@ from ahk import AHK
 
 if __name__ == "__main__":
     ahk = AHK()
-    result = ahk.run_script("DataCommunication.ahk")
-    print(result)
+    data_communication = """#Persistent
+data := "Hello Data!"
+FileAppend, %data%, * ; send data var to stdout
+ExitApp"""
+    result = ahk.run_script(data_communication, decode=False)
+    print(result.stdout)
