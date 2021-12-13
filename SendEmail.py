f019a28f39a013526707b8a78d90adc60a2e943d
collvey <collvey@umd.edu>
2021-12-13 00:30:29 -0800

    Add SendEmail used to send email

---
+++ SendEmail.py
@@ -0,0 +1,34 @@
+import sys
+import datetime
+
+def send_email(user, pwd, recipient, subject, body):
+    import smtplib
+
+    FROM = user
+    TO = recipient if isinstance(recipient, list) else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(user, pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print('successfully sent the mail')
+    except Exception as e:
+        raise(e)
+        print("failed to send mail")
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        body = sys.argv[1]
+    else:
+        body = "Test"
+    now = datetime.datetime.now()
+    cur = now.strftime("%Y-%m-%d %H:%M:%S")
+    send_email("jd.book.006@gmail.com", 'CBh}D"8n', 'collvey.veritas@gmail.com', 'Copied Text at ' + cur, body)
