03fe0e353e342e6f17389624b06cdaabb5da42dd
collvey <collvey@umd.edu>
2021-12-12 23:57:45 -0800

    Use PyAhkExample to show tooltip of clipboard

--- PyAhkExample.ahk
+++ PyAhkExample.ahk
@@ -1,9 +1,10 @@
 #Include RunCmd.ahk
 
 !e:: ; Copy and pop up in MsgBox
-clipboard =
+clipboard :=
 Send, ^c
 ClipWait, 2
-MsgBox, %clipboard%
-RunCmd("python .\ShowTooltipExample.py")
+cmd := "python .\ShowTooltipExample.py " clipboard
+; MsgBox, %cmd%
+RunCmd(cmd)
 return
--- ShowTooltipExample.py
+++ ShowTooltipExample.py
@@ -1,8 +1,10 @@
 from ahk import AHK
 from ahk.window import Window
 
+import sys
+
 if __name__ == "__main__":
   ahk = AHK()
   win = Window.from_mouse_position(ahk)
   win_pos = win.get_pos()
-  ahk.show_tooltip(str(win_pos), second=5, x=win_pos[2]/2, y=win_pos[3]/2)   
+  ahk.show_tooltip(sys.argv[1], second=1, x=win_pos[2]/2, y=win_pos[3]/2)   
